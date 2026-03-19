18c77d6d75d33b0acd1889e4163cd86ebef0b593
Karar Haider <27100468@lums.edu.pk>
2026-03-19 08:12:49 +0500

    Add function prototypes for application

---
+++ app.h
@@ -0,0 +1,20 @@
+/***************************************************************************//**
+ * @file
+ * @brief Function prototypes for the application
+ ******************************************************************************/
+
+#ifndef APP_H
+#define APP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void app_init(void);
+void app_process_action(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // APP_H
